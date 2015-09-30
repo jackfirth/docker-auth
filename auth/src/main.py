e1fc1542c1e886fabd8211b0b4cded5312a82c7b
Jack Firth <jackhfirth@gmail.com>
2015-09-30 11:44:23 -0700

    Add simple proxy route

--- auth/src/main.py
+++ auth/src/main.py
@@ -1,1 +1,13 @@
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    return "Path: %s" % path
+
+
 print("I'm a server!")
