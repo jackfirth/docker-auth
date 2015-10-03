0d8283a066fd0bdfe278a5516a299971f7492cea
Jack Firth <jackhfirth@gmail.com>
2015-10-02 21:53:28 -0700

    Make algorithm configurable, for real this time

--- auth/src/db/crypto.py
+++ auth/src/db/crypto.py
@@ -1,8 +1,8 @@
 from passlib.context import CryptContext
-from config import HASH_ROUNDS
+from config import HASH_ROUNDS, HASH_ALGORITHM
 
 crypt_context = CryptContext(
-    schemes=["pbkdf2_sha512"],
+    schemes=[HASH_ALGORITHM],
     all__vary_rounds=0.1,
     pbkdf2_sha512__default_rounds=HASH_ROUNDS
 )
